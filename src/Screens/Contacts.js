194dfe4984df34b99b74dfbfd96a51e6c1a4f8dc
Yeison Serna <yeisonivanserna@gmail.com>
2021-07-31 15:42:04 -0500

    Modificacion Form - firebase - realtime

---
+++ src/Screens/Contacts.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import firebaseDb from "./firebase";
+import ContactForm from "./ContactForm";
+import formulario from "../Images/union.jpg";
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import UncontrolledLottie2 from "../Components/UncontrolledLottie2";
+
+const Contacts = () => {
+  var [contacObjects, setContactObjects] = useState(0);
+  var [currentId, setCurrentId] = useState("");
+
+  useEffect(() => {
+    firebaseDb.child("contacts").on("value", (snapshot) => {
+      if (snapshot.val() != null)
+        setContactObjects({
+          ...snapshot.val(),
+        });
+    });
+  }, []);
+
+  const addOrEdit = (obj) => {
+    if (currentId == "")
+      firebaseDb.child("contacts").push(obj, (err) => {
+        if (err) console.log(err);
+        else setCurrentId("");
+      });
+    else
+      firebaseDb.child(`contacts/${currentId}`).set(obj, (err) => {
+        if (err) console.log(err);
+        else setCurrentId("");
+      });
+  };
+
+  return (
+    <div className="efecto">
+      <div className="titulo-contacto">
+        <h1 className="animate__animated animate__heartBeat">CONTACTANOS</h1>
+      </div>
+      <div className="formulario">
+        <img src={formulario} alt="formulario"></img>
+      </div>
+
+      <ContactForm {...{ addOrEdit, currentId, contacObjects }} />
+
+      <div className="footer-1">
+        <UncontrolledLottie2 />
+        <p>Contactanos: Whatsapp +57 300 123 4567</p>
+        <p>Siguenos en nuestras redes sociales...</p>
+        <div className="top-redes">
+          <a href="https://www.youtube.com/channel/UC7gilKuSRhW1XyGF-D6LGVg">
+            <FontAwesomeIcon
+              icon={faYoutube}
+              style={{ fontSize: "1.5rem", color: "white" }}
+            />
+          </a>
+
+          <a href="https://www.facebook.com/jduc19/">
+            <FontAwesomeIcon
+              icon={faFacebook}
+              style={{ fontSize: "1.5rem", color: "white" }}
+            />
+          </a>
+
+          <a href="https://www.instagram.com/jduc19/?hl=es-la">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              style={{ fontSize: "1.5rem", color: "white" }}
+            />
+          </a>
+
+          <a href="https://www.instagram.com/jduc19/?hl=es-la">
+            <FontAwesomeIcon
+              icon={faWhatsapp}
+              style={{ fontSize: "1.5rem", color: "white" }}
+            />
+          </a>
+        </div>
+      </div>
+      <footer>
+        <p>Renovemos Colombia</p>
+        <p class="copyrigth">&copy;Todos los derechos reservados</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Contacts;
